b4cbc4985257aca459dfac3e93408218e81d8a16
esyede <esyede@users.noreply.github.com>
2021-09-16 14:19:39 +0000

    phpcs: apply coding style

--- src/Promo/Constants.php
+++ src/Promo/Constants.php
@@ -6,20 +6,20 @@ namespace Esyede\DurianPay\Promo;
 
 class Constants
 {
-    const DISCOUNT_FLAT = 'flat';
-    const DISCOUNT_PERCENTAGE = 'percentage';
+    public const DISCOUNT_FLAT = 'flat';
+    public const DISCOUNT_PERCENTAGE = 'percentage';
 
-    const LIMIT_QUOTA = 'quota';
-    const LIMIT_BUDGET = 'budget';
+    public const LIMIT_QUOTA = 'quota';
+    public const LIMIT_BUDGET = 'budget';
 
-    const DEDUCTION_TOTAL_PRICE = 'total_price';
-    const DEDUCTION_PRODUCT_PRICE = 'product_price';
-    const DEDUCTION_SHIPPING_PRICE = 'shipping_price';
+    public const DEDUCTION_TOTAL_PRICE = 'total_price';
+    public const DEDUCTION_PRODUCT_PRICE = 'product_price';
+    public const DEDUCTION_SHIPPING_PRICE = 'shipping_price';
 
-    const PROMO_TYPE_CARD = 'card_promos';
-    const PROMO_TYPE_EWALLET = 'ewallet_promos';
-    const PROMO_TYPE_VA = 'va_promos';
+    public const PROMO_TYPE_CARD = 'card_promos';
+    public const PROMO_TYPE_EWALLET = 'ewallet_promos';
+    public const PROMO_TYPE_VA = 'va_promos';
 
-    const PROMO_SUBTYPE_DIRECT_DISCOUNT = 'direct_discount';
-    const PROMO_SUBTYPE_CASHBACK = 'cashback';
+    public const PROMO_SUBTYPE_DIRECT_DISCOUNT = 'direct_discount';
+    public const PROMO_SUBTYPE_CASHBACK = 'cashback';
 }
